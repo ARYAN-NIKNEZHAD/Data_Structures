b19eb7a866061cb5dea92660b23d02b03f47a5b0
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-03-18 11:36:19 +0330

    Add-Stack-file

---
+++ Code/Stack/Stack.py
@@ -0,0 +1,25 @@
+
+
+class Stack:
+
+    def __init__(self, limit) -> None:
+        self.stack = []
+        self.limit = limit
+
+    def top(self):
+        if len(self.stack) <= 0:
+            return -1
+        else:
+            return self.stack[len(self.stack) - 1]
+        
+    def push(self, data):
+        if len(self.stack) >= self.limit:
+            return -1
+        else:
+            self.stack.append(data)
+    
+    def pop(self):
+        if len(self.stack) <= 0:
+            return -1 
+        else:
+            return self.stack.pop()
